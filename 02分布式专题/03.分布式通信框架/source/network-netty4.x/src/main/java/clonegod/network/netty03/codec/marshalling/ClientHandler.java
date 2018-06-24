bb53dec0cfd4a6c61f07ca404d3c095ee1073e2b
clonegod <asynclife@163.com>
2018-06-25 03:20:16 +0800

    netty

---
+++ 02分布式专题/03.分布式通信框架/source/network-netty4.x/src/main/java/clonegod/network/netty03/codec/marshalling/ClientHandler.java
@@ -0,0 +1,34 @@
+package clonegod.network.netty03.codec.marshalling;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+	
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		try {
+			RPCResponse resp = (RPCResponse)msg;
+			System.out.println("Client : " + resp.toString());			
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		ctx.close();
+	}
+	
+}
